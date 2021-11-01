9f0ea1c5ea719a66bebf8e7102f7affaa41ca4b8
Porson <porsonlee@gmail.com>
2021-11-01 15:29:55 -0700

    deleted css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 // import './App.css';
+
+// Components
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contacts/Contact";
+
+// scss
+import "./app.scss";
 
 function App() {
   return (
