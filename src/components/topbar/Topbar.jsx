d1574199c0fc7fc6ff7f26e4d945d84e46a50950
Porson <porsonlee@gmail.com>
2021-11-02 13:36:55 -0700

    add temporary logo name to left nav bar. removed anchored link and set link text color to inherit

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -5,9 +5,9 @@ export default function Topbar() {
   <div className="topbar">
     <div className="wrapper">
       <div className="left">
-      <a href="#intro">porson lee</a>
+      <a href="#intro" className="logo">porsonLee</a>
       </div>
-      <h1>Hello.</h1>
+      {/* <h1>Hello.</h1> */}
       <div className="right">this is right</div>
     </div>
   </div>
