af07a29adb82bad2d9d2bbff189ce020e7fb202f
Porson <porsonlee@gmail.com>
2021-11-01 21:59:26 -0700

    created wrapper inside the nav bar to create left and right div. left div will be logo and right div will be hamburger menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,14 @@
 import "./topbar.scss";
 
 export default function Topbar() {
-  return <div className="topbar">top bar</div>;
+  return (
+  <div className="topbar">top bar
+    <div className="wrapper">
+      <div className="left">
+      <a href="#intro">porson lee</a>
+      </div>
+      <div className="right"></div>
+    </div>
+  </div>
+  )
 }
