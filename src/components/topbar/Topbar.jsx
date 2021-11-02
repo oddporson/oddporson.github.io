489917b6d61953b39ec69f5a51c55d739e3815bc
Porson <porsonlee@gmail.com>
2021-11-02 15:22:49 -0700

    contact info are now display flex, items center aligned and margin spaced

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -6,17 +6,16 @@ export default function Topbar() {
   <div className="topbar">
     <div className="wrapper">
       <div className="left">
-        <a href="#intro" className="logo">porsonLee</a>"
+        <a href="#intro" className="logo">porsonLee</a>
         <div className="itemContainer">
-        <Person className="icon"/>
-        <span>778-123-4567</span>
+          <Person className="icon"/>
+          <span>778-123-4567</span>
         </div>
         <div className="itemContainer">
-        <Mail className="icon"/>
-        <span>porsonlee@gmail.com</span>
+          <Mail className="icon"/>
+          <span>porsonlee@gmail.com</span>
         </div>
       </div>
-      {/* <h1>Hello.</h1> */}
       <div className="right">this is right</div>
     </div>
   </div>
