1f2aebfa12b94e47d48459a05cdc86aae192ecc8
Porson <porsonlee@gmail.com>
2021-11-02 15:16:37 -0700

    added contact info but showing scrollbar in navigation which i think the topbar scss is not display flex

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,11 +1,20 @@
 import "./topbar.scss";
+import {Person, Mail} from "@material-ui/icons"
 
 export default function Topbar() {
   return (
   <div className="topbar">
     <div className="wrapper">
       <div className="left">
-      <a href="#intro" className="logo">porsonLee</a>
+        <a href="#intro" className="logo">porsonLee</a>"
+        <div className="itemContainer">
+        <Person className="icon"/>
+        <span>778-123-4567</span>
+        </div>
+        <div className="itemContainer">
+        <Mail className="icon"/>
+        <span>porsonlee@gmail.com</span>
+        </div>
       </div>
       {/* <h1>Hello.</h1> */}
       <div className="right">this is right</div>
