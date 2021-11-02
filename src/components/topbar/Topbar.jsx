ea11ba6381c26b1d7b3667372e54ee5e11da29d2
Porson <porsonlee@gmail.com>
2021-11-01 22:20:07 -0700

    left element start from the beginning, right element start at the far right

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,12 +2,13 @@ import "./topbar.scss";
 
 export default function Topbar() {
   return (
-  <div className="topbar">top bar
+  <div className="topbar">
     <div className="wrapper">
       <div className="left">
       <a href="#intro">porson lee</a>
       </div>
-      <div className="right"></div>
+      <h1>Hello</h1>
+      <div className="right">this is right</div>
     </div>
   </div>
   )
