2f32a213b76d23e50f0eb89c2247374f52666683
Porson <porsonlee@gmail.com>
2021-11-03 11:38:53 -0700

    added photo of my bboy pose and align photo to be properly centered in left div

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,14 @@
 import "./intro.scss";
 
 export default function Intro() {
-  return <div className="intro" id="intro">intro</div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/airbaby.png" alt="bboy move" />
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
 }
