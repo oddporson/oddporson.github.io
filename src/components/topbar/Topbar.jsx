495c243ee8d6eb3a5117a9b760e81f0a37803b44
Porson <porsonlee@gmail.com>
2021-11-03 13:27:25 -0700

    adjusted arrow again

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            porsonlee.
+            porsonLee
           </a>
           <div className="itemContainer">
             <Person className="icon" />
