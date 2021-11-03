35bee4e8888616ba64590e4205bd0f72d42fae9d
Porson <porsonlee@gmail.com>
2021-11-03 10:48:27 -0700

    updated fake number for now. still a debate whether to include real number or not

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,29 +1,31 @@
 import "./topbar.scss";
-import {Person, Mail} from "@material-ui/icons"
+import { Person, Mail } from "@material-ui/icons";
 
-export default function Topbar({menuOpen, setMenuOpen}) {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-  <div className= {"topbar " + (menuOpen && "active")}>
-    <div className="wrapper">
-      <div className="left">
-        <a href="#intro" className="logo">porsonlee.</a>
-        <div className="itemContainer">
-          <Person className="icon"/>
-          <span>778-123-4567</span>
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            porsonlee.
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>123-456-7890</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>porsonlee@gmail.com</span>
+          </div>
         </div>
-        <div className="itemContainer">
-          <Mail className="icon"/>
-          <span>porsonlee@gmail.com</span>
-        </div>
-      </div>
-      <div className="right">
-        <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
-          <span className="line1"></span>
-          <span className="line2"></span>
-          <span className="line3"></span>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
