4a8120817bc627081405fc12798274748eefdcf0
Porson <porsonlee@gmail.com>
2021-11-03 13:04:30 -0700

    upload photos and added text to right div

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -8,7 +8,18 @@ export default function Intro() {
           <img src="assets/airbaby.png" alt="bboy move" />
         </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <div className="wrapper">
+          <h1>hello world.</h1>
+          <h2>I'm Porson Lee</h2>
+          <h3>
+            Freelance <span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="" alt="" />
+        </a>
+      </div>
     </div>
   );
 }
