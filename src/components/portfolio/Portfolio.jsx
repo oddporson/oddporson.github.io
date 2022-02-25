04d827d4584a1f7210c919e7dc64e53b152e7b99
Porson <porsonlee@gmail.com>
2022-02-25 00:21:55 -0800

    to be continued with having user click on portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -11,20 +11,20 @@ export default function Portfolio() {
       </ul>
       <div className="container">
         <div className="item">
-          <img src="assets/placehoflder-img.png" alt="tweeter" />
-          <h3>Tweeter</h3>
+          <img src="assets/Tripper.png" alt="traveling app" />
+          <h3>Travelling App</h3>
         </div>
         <div className="item">
-          <img src="assets/placeholder-img.png" alt="react scheduler" />
-          <h3>React Scheduler</h3>
-        </div>
-        <div className="item">
-          <img src="assets/placeholdesr-img.png" alt="pizza shop" />
+          <img src="assets/LHL-Pizza.jpg" alt="pizza shop" />
           <h3>Pizza Shop</h3>
         </div>
         <div className="item">
-          <img src="assets/placseholder-img.png" alt="traveling app" />
-          <h3>Travelling App</h3>
+          <img src="assets/React-Scheduler.jpg" alt="react scheduler" />
+          <h3>React Scheduler</h3>
+        </div>
+        <div className="item">
+          <img src="assets/Tweeter.png" alt="tweeter" />
+          <h3>Tweeter</h3>
         </div>
         {/* <div className="item">
           <img src="assets/placefholder-img.png" alt="tiny app" />
