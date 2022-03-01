3259f160374960972e96a1f3c1dcc78b44a7838d
Porson <porsonlee@gmail.com>
2022-03-01 14:18:41 -0800

    added professional work and testimonial

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,17 +1,43 @@
-import { Mail } from "@material-ui/icons";
-
 import "./portfolio.scss";
 
 const Portfolio = () => {
   return (
     <div className="portfolio" id="portfolio">
+      <h1>Work</h1>
+      <div className="top">
+        <div className="left">
+          <a href="https://www.dukesfineicecream.com/">
+            <img
+              src="assets/dukesfineicecream.png"
+              alt="dukes fine ice cream"
+            />
+          </a>
+        </div>
+        <div className="right">
+          <div className="title">
+            <h1>Duke's Fine Ice Cream</h1>
+            <p>Surrey, BC, Canada</p>
+          </div>
+          <div className="testimonial">
+            <h2>Testimonial</h2>
+            <p>
+              "Working with Porson was great - he's easy to talk to, very
+              professional, artistic and had a clear vision to make us look
+              slick, modern, and terrific! He listened to our needs and had
+              options to meet those needs, as well as options for us to access
+              the ongoing back-end data that keeps us informed. He's an
+              accomplished photographer who creates images that look way better
+              than anything we could have imagined. Friendly, prompt, and
+              accurate, Porson gets the job done, and is easy to contact
+              whenever adjustments are needed. We highly recommend his
+              services!"
+            </p>
+            <span>- Crystal Lynn Guay (Duke's Fine Ice Cream Owner)</span>
+          </div>
+        </div>
+      </div>
       <h1>Projects</h1>
-      <ul>
-        {/* <li className="active">Featured</li> */}
-        {/* <li>Web App</li>
-        <li>Design</li> */}
-      </ul>
-      <div className="container">
+      <div className="bottom">
         <div className="item">
           <img src="assets/Tripper.png" alt="traveling app" />
           <h3>Travelling App</h3>
@@ -27,18 +53,6 @@ const Portfolio = () => {
         <div className="item">
           <img src="assets/Tweeter.png" alt="tweeter" />
           <h3>Tweeter</h3>
-        </div>
-      </div>
-      <div className="footer">
-        <div className="itemContainer">
-          <img src="assets/github.png" alt="github" />
-          <a href="https://github.com/oddporson">
-            <span>oddporson</span>
-          </a>
-        </div>
-        <div className="itemContainer">
-          <Mail className="icon" />
-          <span>porsonlee@gmail.com</span>
         </div>
       </div>
     </div>
